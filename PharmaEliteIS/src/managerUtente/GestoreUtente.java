b8d9715b76849a42081c8400c637dde693ab6b97
Luigi-Bozzoli <aloysiusbozzoli@gmail.com>
2019-12-17 08:45:31 +0100

    m

--- PharmaEliteIS/src/managerUtente/GestoreUtente.java
+++ PharmaEliteIS/src/managerUtente/GestoreUtente.java
@@ -2,4 +2,14 @@ package managerUtente;
 
 public class GestoreUtente {
 
+	public void login(ClienteBean c) {
+		
+	}
+	
+	public void registrazione(ClienteBean c) {
+		
+	}
+	
+	
+	//public void userpage(Http)
 }
